777c9a62229f9b559123d839f02f46acb6ae19ee
Arava Choudhary <aravachoudhary11@gmail.com>
2025-02-02 18:36:21 +0530

    Frontend Configurations

--- second/second.js
+++
@@ -1,8 +0,0 @@
-function nextPage(pageNumber) {
-    document.querySelectorAll('.page').forEach(page => page.classList.add('d-none'));
-    document.getElementById(`page${pageNumber}`).classList.remove('d-none');
-}
-
-function restart() {
-    window.location.href = "../index.html"; // Go back to the first page
-}
